5936813ab57de8656802e81ba7c2f68b5acedc3e
chrscato <christophercato7@gmail.com>
2025-04-30 15:04:25 -0400

    Deploy: Wed, Apr 30, 2025  3:04:25 PM

--- portal/views/home.py
+++ portal/views/home.py
@@ -1,9 +1,7 @@
 from flask import Blueprint, render_template
-from flask_login import login_required
 
 home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/')
-@login_required
 def home():
     return render_template('home.html')
